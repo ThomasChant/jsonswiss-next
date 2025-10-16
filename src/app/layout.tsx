3c3e49c35ece441de48740250ff1e2d66d737e52
ThomasChant <xgct0505@qq.com>
2025-10-16 14:08:09 +0800

    feat(layout): add Clarity tracking code for enhanced user insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,14 +80,27 @@ export default function RootLayout({
               dangerouslySetInnerHTML={{
                 __html: `
                   window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
+                  function gtag(){dataLayer.push(arguments);} 
                   gtag('js', new Date());
                   gtag('config', 'G-N1WNYVYMNW');
                 `,
               }}
             />
+            {/* Clarity tracking code for https://jsonswiss.com/ */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(c,l,a,r,i,t,y){
+                      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "tqxpvmick3");
+                `,
+              }}
+            />
           </>
         )}
+
       </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased min-h-screen`}
